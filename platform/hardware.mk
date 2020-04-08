4e5bcb44b8cb0e277abcd5c61beb1acdc91f282d
Roman Kiryanov <rkir@google.com>
2020-04-08 14:23:47 -0700

    yoshino-common: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -74,7 +74,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl:32
 
 # BLUETOOTH
