1f9ffbdfbfb8112a28020f0146e29a9f687e39cf
Vinoth Jayaram <quic_c_vinoja@quicinc.com>
2022-12-23 18:14:42 +0530

    yoshino-common: gralloc: Fix Compilation errors
    Include -Wno-sign-conversion flag to avoid sign conversion warning.
    
    Change-Id: I576ec57856ac1cab25ca657dcadbfbfa1dddf33c
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- hardware/qcom-caf/display/common.mk
+++ hardware/qcom-caf/display/common.mk
@@ -16,7 +16,7 @@ display_config_version := $(shell \
 
 #Common C flags
 common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
-common_flags += -Wconversion -Wall -Werror
+common_flags += -Wconversion -Wall -Werror -Wno-sign-conversion
 common_flags += -DUSE_GRALLOC1
 ifeq ($(TARGET_IS_HEADLESS), true)
     common_flags += -DTARGET_HEADLESS
