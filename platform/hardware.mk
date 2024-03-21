17cc1f6849f67a37f56038848027972422008f7e
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    yoshino-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -184,8 +184,6 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0 \
-    android.hardware.usb@1.1 \
     android.hardware.usb@1.3-service.dual_role_usb
 
 # VIBRATOR
