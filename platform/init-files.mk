5e8ce7025a5ba0aad81564415f941444862f483c
Michael Bestas <mkbestas@lineageos.org>
2024-12-13 05:12:05 +0000

    yoshino-common: Convert init Android.mk to bp
    Change-Id: I43ccc55b98231d984474e6bba3c9fa1266bb7351

--- platform/init-files.mk
+++ platform/init-files.mk
@@ -1,17 +1,24 @@
+# fstab
+PRODUCT_PACKAGES += \
+    fstab.yoshino
+
 # init rc
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/init/init.yoshino.ims.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.ims.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.pwr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.pwr.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.qcom.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.srv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.srv.rc \
-    $(PLATFORM_PATH)/config/init/init.yoshino.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yoshino.usb.rc
+PRODUCT_PACKAGES += \
+    init.yoshino.rc \
+    init.yoshino.ims.rc \
+    init.yoshino.pwr.rc \
+    init.yoshino.qcom.rc \
+    init.yoshino.srv.rc \
+    init.yoshino.usb.rc
 
 # init.qcom.early_boot.sh (modified)
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/init/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
-    $(PLATFORM_PATH)/config/init/init.qcom.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.radio.sh
+PRODUCT_PACKAGES += \
+    init.qcom.early_boot.sh \
+    init.qcom.radio.sh
 
 # ueventd
+PRODUCT_PACKAGES += \
+    ueventd.rc
+
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
     $(PLATFORM_PATH)/config/init/ueventd.yoshino.rc:$(TARGET_COPY_OUT_VENDOR)/odm/etc/ueventd.rc
